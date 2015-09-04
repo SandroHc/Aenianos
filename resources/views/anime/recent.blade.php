cd5873f69b4f658b24808473b1cec593ea3057ad
Sandro Amaral <sandro123iv@gmail.com>
2015-09-04 03:46:09 +0100

    Reworked the Latest Releases section in the home page.

--- resources/views/anime/recent.blade.php
+++ resources/views/anime/recent.blade.php
@@ -1,11 +1,15 @@
 <div id="spotlight">
+	<p>LANÇAMENTOS RECENTES</p>
+
 	@foreach(\App\Models\Episode::getLatestEpisodes() as $data)
 		<div class="spotlight-content">
 			<a class="spotlight-link" href="{!! URL::action('AnimeController@showDetailSlug', [ 'slug' => $data->slug ]) !!}" target="_self">
 				<img class="spotlight-img" src="{{ !empty($data->official_cover) ? $data->official_cover : (!empty($data->cover) ? $data->cover : '/img/unknown.png') }}">
+				<div class="spotlight-overlay"></div>
 
 				<div class="spotlight-description">
-					{{ \App\Util::trim($data->title, 20) }}
+					{{ \App\Util::trim($data->title, 20) }}<br>
+					<div class="spotlight-episode">Ep. ?</div>
 				</div>
 			</a>
 		</div>
